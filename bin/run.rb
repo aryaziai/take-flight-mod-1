ecbc902243a0a7087277ad42c1cafe2f5b2d23eb
aryaziai <54697831+aryaziai@users.noreply.github.com>
2019-11-12 13:07:15 -0800

    Update run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,24 @@
-require_relative '../config/environment'
+require_relative '../config/environment.rb'
+require_relative '../app/models/cli.rb'
 
-puts "hello world, how is it going"
+arya = TakeFlight.new
+
+user_input = nil
+until user_input == "quit"
+    arya.greetings
+    arya.waiting
+    arya.add_customer
+    arya.domestic_international
+    arya.dest
+    arya.seating
+    arya.goodbye
+    puts "\nBooking Agent: Would you like to purchase another ticket? 'Yes' or 'No'\n\n"
+    user_input = gets.chomp
+    if user_input == 'no' or user_input == "No" or user_input == "exit" or user_input == "Exit" or user_input == "quit" or user_input == "Quit"
+        break
+    end
+end
+
+#binding.pry
+
+
